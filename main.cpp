9278a5167c2796b35e3df2454e2b8c87f7256d14
GergelyPiltz <112201162+GergelyPiltz@users.noreply.github.com>
2023-09-16 19:36:16 +0200

    hibaakjelolese

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+constexpr int N_ELEMENTS = 100; //constexpr?
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int* b = new int[NELEMENTS]; //hiányzik _
+    std::cout << '1-100 ertekek duplazasa' // '' helyett "" és sor nincs lezárva és std::endl;
+        for (int i = 0;) //hiányzik a ciklus léptetés, feltétel
+        {
+            b[i] = i * 2; // b egy pointer tömb
+        }
+    for (int i = 0; i; i++) //hiányzik a feltétel
     {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:" // sor nincs lezárva és std::endl;
+    } // a két for ciklusnak egynek kéne lennie
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //nincs kezdőérték és float vagy double kell
+    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] // sor nincs lezárva
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
